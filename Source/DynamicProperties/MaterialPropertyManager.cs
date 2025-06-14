93c78d400c62c44d4f10f49fb07c96e066270e5c
Alvin Meng <alvin.q.meng@gmail.com>
2025-06-14 14:55:13 -0400

    patch stock fairings

--- Source/DynamicProperties/MaterialPropertyManager.cs
+++ Source/DynamicProperties/MaterialPropertyManager.cs
@@ -46,6 +46,7 @@ public sealed class MaterialPropertyManager : MonoBehaviour
 		PartPatch.ClearOnSceneSwitch();
 		MaterialColorUpdaterPatch.ClearOnSceneSwitch();
 		ModuleColorChangerPatch.ClearOnSceneSwitch();
+		FairingPanelPatch.ClearOnSceneSwitch();
 
 		this.LogMessage("destroyed");
 	}
